fc3bf82491eb83a78330b2ed42d8c74a75d88e55
Jonniesam <117244803+Jonniesam@users.noreply.github.com>
2022-11-27 16:45:20 -0800

    Added equation to calculate cell pixel and gridsize, added test buttons

--- e-a-s.js
+++ e-a-s.js
@@ -1,23 +1,68 @@
 const container = document.getElementById("container");
 const cell = document.createElement('div');
+const big_button = document.getElementById('big');
+const med_button = document.getElementById('med');
+const small_button = document.getElementById('small');
+const clear_button = document.getElementById('clear');
 
 
+big_button.addEventListener('click', function () {
+    grid(64);
+});
 
-function grid() {
+med_button.addEventListener('click', function () {
+    grid(32);
+});
+
+small_button.addEventListener('click', function () {
+    grid(16);
+});
 
 
-
-    for(let i = 1; i <= 256; i++){
+function first() {
+    let gridSize = (16 * 16);
+        let containerSize = 500
+        let cellPixel = Math.sqrt(((containerSize * containerSize)/ gridSize)) - 2
+    for(let i = 1; i <= gridSize; i++){
         const cell = document.createElement('div');
-cell.innerHTML = ""
-cell.style.border = "solid";
-cell.style.backgroundColor = "white";
-cell.id ="cell";
-container.appendChild(cell); 
-    }
+        cell.style.border = "solid";
+        cell.style.borderWidth = "1px";
+        cell.style.backgroundColor = "white";
+        cell.id ="cell";
+        cell.style.width = `${cellPixel}px`;
+        cell.style.height = `${cellPixel}px`;
+        container.appendChild(cell); 
+}
 };
 
-grid()
+
+
+function grid(size) {
+    if(size === undefined){
+        first();
+    } else {
+        let hw = size;
+        console.log(size);
+        let gridSize = (hw * hw);
+        let containerSize = 500
+        let cellPixel = Math.sqrt(((containerSize * containerSize)/ gridSize)) - 2;
+    for(let i = 1; i <= gridSize; i++){
+        const container = document.getElementById("container");
+        const cell = document.createElement('div');
+        cell.style.border = "solid";
+        cell.style.borderWidth = "1px";
+        cell.style.backgroundColor = "white";
+        cell.id ="cell";
+        cell.style.width = `${cellPixel}px`;
+        cell.style.height = `${cellPixel}px`;
+        container.appendChild(cell); 
+    }
+    
+}
+}
+
+grid();
+
 
 container.addEventListener('mousemove', changeColor);
 function changeColor(e) {
